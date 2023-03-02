f2b262e39c55bc4b1404748e5f16fdeae00cfe6f
Rahul Yadav <ryrahul12345@gmail.com>
2023-03-02 20:44:59 +0530

    updated

--- BT/diameter.cpp
+++ BT/diameter.cpp
@@ -1,40 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-class node{
-    public:
-        int data;
-        node *left;
-        node *right;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
 
-    node(int val){
+    node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
-int heightOfTree(node* root){
-    if(root == NULL)
+int heightOfTree(node *root)
+{
+    if (root == NULL)
         return 0;
     int lheight = heightOfTree(root->left);
     int rheight = heightOfTree(root->right);
-    int ans = max(lheight,rheight) + 1;
+    int ans = max(lheight, rheight) + 1;
 
     return ans;
 }
-int diameterOfBinaryTree(node* root){
+
+// its time complexity is O(n^2)
+int diameterOfBinaryTree(node *root)
+{
     // base case
-    if(root == NULL)
+    if (root == NULL)
         return 0;
-    
+
     int op1 = diameterOfBinaryTree(root->left);
     int op2 = diameterOfBinaryTree(root->right);
-    int op3 = heightOfTree(root->left) + heightOfTree(root->right) +1;
+    int op3 = heightOfTree(root->left) + heightOfTree(root->right) + 1;
 
-    int ans = max(op3,(max(op1,op2)));
-    
+    int ans = max(op3, (max(op1, op2)));
+
     return ans;
-
 }
+
+// Its time complexity is O(n)
+pair<int, int> fastDiameter(node *root)
+{
+    if (root == NULL)
+    {
+        pair<int, int> p = make_pair(0, 0);
+        return p;
+    }
+
+    pair<int, int> left = fastDiameter(root->left);
+    pair<int, int> right = fastDiameter(root->right);
+
+    int op1 = left.first;
+    int op2 = right.first;
+    int op3 = left.second + right.second + 1;
+
+    pair<int, int> ans;
+    ans.first = max(op3, (max(op1, op2)));
+    ans.second = max(left.second, right.second) + 1;
+    return ans;
+}
+
 int main()
 {
     node *root = NULL;
