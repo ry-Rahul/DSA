95275042a3b75cff8402ac160da572c615e7fce2
Rahul Yadav <ryrahul12345@gmail.com>
2023-03-02 18:51:42 +0530

    updated

--- BT/heightBinary.cpp
+++ BT/heightBinary.cpp
@@ -1,13 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define vi vector<int>
-#define vvi vector<vi>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define rep(i, a, b) for (int i = a; i < b; i++)
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int heightOfTree(node* root){
+    if(root == NULL)
+        return 0;
+    int lheight = heightOfTree(root->left);
+    int rheight = heightOfTree(root->right);
+    int ans = max(lheight,rheight) + 1;
+
+    return ans;
+}
 
 int main()
 {
-
+  node* root = NULL:
     return 0;
 }
