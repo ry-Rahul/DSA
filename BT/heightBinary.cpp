54c66d058028722c7697427ffbdc7bf96a317f38
Rahul Yadav <ryrahul12345@gmail.com>
2023-03-02 19:57:23 +0530

    update

--- BT/heightBinary.cpp
+++ BT/heightBinary.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-class node{
-    public:
-        int data;
-        node *left;
-        node *right;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
 
-    node(int val){
+    node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
-int heightOfTree(node* root){
-    if(root == NULL)
+int heightOfTree(node *root)
+{
+    if (root == NULL)
         return 0;
     int lheight = heightOfTree(root->left);
     int rheight = heightOfTree(root->right);
-    int ans = max(lheight,rheight) + 1;
+    int ans = max(lheight, rheight) + 1;
 
     return ans;
 }
 
 int main()
 {
-  node* root = NULL:
+    node *root = NULL;
     return 0;
 }
