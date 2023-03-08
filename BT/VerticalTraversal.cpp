28fe381a22eec0692c74f50e06ca4cbd7b1ff19f
Rahul Yadav <ryrahul12345@gmail.com>
2023-03-08 12:53:04 +0530

    update

---
+++ BT/VerticalTraversal.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+vector<int> verticalOrder(node *root)
+{
+    map<int, map<int, vector<int>>> nodes;
+    queue<pair<node *, pair<int, int>>> q;
+    vector<int> ans;
+
+    if (root == NULL)
+        return ans;
+
+    q.push(make_pair(root, make_pair(0, 0)));
+
+    while (!q.empty())
+    {
+        pair<node *, pair<int, int>> temp = q.front();
+        q.pop();
+        node *frontNode = temp.first;
+        int hd = temp.second.first;
+        int lvl = temp.second.second;
+
+        nodes[hd][lvl].push_back(frontNode->data);
+
+        if (frontNode->left)
+            q.push(make_pair(frontNode->left, make_pair(hd - 1, lvl + 1)));
+
+        if (frontNode->right)
+            q.push(make_pair(frontNode->right, make_pair(hd + 1, lvl + 1)));
+    }
+
+    for (auto i : nodes)
+    {
+
+        for (auto j : i.second)
+        {
+
+            for (auto k : j.second)
+            {
+                ans.push_back(k);
+            }
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->left->left->left = new node(8);
+    return 0;
+}
