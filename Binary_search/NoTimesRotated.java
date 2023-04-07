0732896cf4fd1a0ce20fbdac537404db075d8714
Rahul Yadav <ryrahul12345@gmail.com>
2023-04-07 23:41:27 +0530

    minElementInSortenArray

---
+++ Binary_search/NoTimesRotated.java
@@ -0,0 +1,33 @@
+
+public class NoTimesRotated {
+
+    static public int NoOfTimesRotated(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        int size = arr.length, next, prev;
+        int mid;
+
+        while (start < end) {
+            mid = start + (end - start) / 2;
+            
+            if (arr[mid] > arr[end]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+
+        }
+
+        return arr[start];
+    }
+
+    public static void main(String[] args) {
+        // 12,15,18,22,25,30,45,55,62,75,2,5,8,9,11
+        int arr[] = { 12,15,18,22,25,30,45,55,62,75,2,5,8,9,11};
+
+        int ans = NoOfTimesRotated(arr);
+        System.out.println("Minimum Element in rotated Sorted Array is " + ans);
+
+    }
+}
