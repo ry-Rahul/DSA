ca9e01233c1801b1d885981d56d72b0edcc3d955
Rahul Yadav <ryrahul12345@gmail.com>
2023-04-13 02:33:01 +0530

    updated

--- LeetCode/BSearch/perfectSqrOrNot.java
+++ LeetCode/BSearch/perfectSqrOrNot.java
@@ -1,22 +1,19 @@
 public class perfectSqrOrNot {
     public static boolean isPerfectSquare(int num) {
-        int st = 1;
-        int en = num;
-        while (st <= en) {
-            int mid = st + (en - st) / 2;
-            if (mid * mid == num) {
-                return true;
-            } else if (mid * mid < num) {
-                st = mid + 1;
-            } else {
-                en = mid - 1;
-            }
-        }
-        return false;
-    }
+        int low = 1;
+        int high = num;
+ 
+      while (low <= high) {
+         long mid = (low + high) / 2;
+         if (mid * mid == num) return true;
+         else if (mid * mid < num) low = (int) mid + 1;
+         else high = (int) mid - 1;
+     }
+         return false;
+     }
 
     public static void main(String[] args) {
-        int num = 25;
+        int num = 9;
         boolean ans = isPerfectSquare(num);
         System.out.println(ans);
     }
