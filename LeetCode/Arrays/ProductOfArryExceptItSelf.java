2c1e1b31f28e8537498b73121986a1fd1a787bcf
Rahul Yadav <ryrahul12345@gmail.com>
2023-04-13 23:50:14 +0530

    updated

--- LeetCode/Arrays/ProductOfArryExceptItSelf.java
+++ LeetCode/Arrays/ProductOfArryExceptItSelf.java
@@ -27,8 +27,27 @@ public class ProductOfArryExceptItSelf {
         return ans;
     }
 
+    // method 2
+    public static int[] productOfArray2(int[] arr) {
+        int n = arr.length;
+        int ans[] = new int[n];
+
+        int right = 1;
+        ans[0] = 1;
+        for (int i = 1; i < n; i++)
+            ans[i] = ans[i - 1] * arr[i - 1];
+
+        for (int j = n - 1; j >= 0; j--)
+        {
+            ans[j] = ans[j] * right;
+            right = right * arr[j];
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4 };
+        int arr[] = { 4,5,1,8,2 };
         System.out.println("The product of array is: " + Arrays.toString(productOfArray(arr)));
     }
 }
