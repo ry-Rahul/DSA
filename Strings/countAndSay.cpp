0c5e14d9af8fd08b8fa58eb529a712c8502d84dc
Rahul Yadav <ryrahul12345@gmail.com>
2023-09-25 23:54:45 +0530

    updated

--- Strings/countAndSay.cpp
+++ Strings/countAndSay.cpp
@@ -9,21 +9,13 @@ public:
             return "1";
         }
 
-        string prev = countAndSay(n-1);
+        string say = countAndSay(n-1);
         string ans = "";
         int count = 1;
-        for(int i=1; i<prev.size(); i++){
-            if(prev[i] == prev[i-1]){
-                count++;
-            }else{
-                ans += to_string(count);
-                ans += prev[i-1];
-                count = 1;
-            }
+        for(int i=1; i<say.length(); i++){
+            
         }
-        ans += to_string(count);
-        ans += prev[prev.size()-1];
-        return ans;
+        
 
     }
 };
